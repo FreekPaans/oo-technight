ba7c7129112f501150912775e61794f5d80d1949
Freek Paans <freekpaans@gmail.com>
2015-07-16 17:12:28 +0200

    ready for session

--- javascript/code/circuit-simulation.js
+++ javascript/code/circuit-simulation.js
@@ -1,31 +1,8 @@
 console.log("loading circuit-simulation.js")
 
-module.exports.Wire = function() {
-	var signal = 0;
-
-	var actions = [];
-
-	var addAction = function(action) {
-		actions.push(action)
-	}
-
-	var setHigh = function() {
-		signal = 1;
-		actions.forEach(function(action) {
-			action();
-		})
-	}
-
+var Wire = function() {
 	return function(op) {
 		switch(op) {
-			case "set-high":
-				setHigh();
-				return;
-			case "get-signal":
-				return signal;
-			case "add-action":
-				addAction(arguments[1])
-				return;
 			default:
 				throw Error("Unknown msg "+op)
 
@@ -33,13 +10,27 @@ module.exports.Wire = function() {
 	};
 }
 
-module.exports.OrGate = function(in1, in2, out) {
-	var onChange = function() {
-		if(in1("get-signal") || in2("get-signal")) {
-			out("set-high")
-		}
-	}
-	in1("add-action", onChange)
-	in2("add-action", onChange)
+var OrGate = function(in1, in2, out) {
 	return function() {};
 }
+
+var AndGate = function(in1, in2, out) {
+	return function() {};
+}
+
+var Inverter = function(inp, out) {
+	return function() {};
+}
+
+var HalfAdder = function(A, B, S, C) {
+	var D = Wire();
+	var E = Wire();
+	OrGate(A, B, D);
+	AndGate(A,B, C);
+	Inverter(C, E);
+	AndGate(D, E, S);
+}
+
+module.exports.Wire = Wire;
+module.exports.OrGate = OrGate;
+module.exports.HalfAdder = HalfAdder;
--- javascript/spec/tests/circuit-simulation-spec.js
+++ javascript/spec/tests/circuit-simulation-spec.js
@@ -2,46 +2,77 @@ var circuit = require('../../code/circuit-simulation.js');
 
 var Wire = circuit.Wire;
 var OrGate=  circuit.OrGate;
+var HalfAdder = circuit.HalfAdder;
 
-describe("a wire", function() {
-	it("should start low", function() {
-		var wire = Wire();
-		expect(wire("get-signal")).toBe(0)
+describe("an half adder", function() {
+	var inA,inB,outS,outC;
+	
+	beforeEach(function() {
+		inA = Wire();
+		inB = Wire();
+		outS = Wire();
+		outC = Wire();
+		HalfAdder(inA, inB, outS, outC);
 	})
+
+	var truthTable = [
+	  // A B S C
+		[0,0,0,0],
+		[0,1,0,1],
+		[1,0,0,1],
+		[1,1,1,0],
+	]
+
+	truthTable.forEach(function(row) {
+		it(JSON.stringify(row), function() {
+			inA("set-signal", row[0]);
+			inB("set-signal", row[1]);
+			expect(outS("get-signal")).toBe(row[2], "S");
+			expect(outC("get-signal")).toBe(row[3], "C");
+		});
+	})
+
+
+	// describe("a wire", function() {
+	// 	it("should start low", function() {
+	// 		var wire = Wire();
+	// 		expect(wire("get-signal")).toBe(0)
+	// 	})
+	// })
+
+	// describe("an OR gate", function() {
+	// 	var in1,in2,out;
+	// 	describe("when in1 and in2 start low", function() {
+	// 		beforeEach(function() {
+	// 			in1 = Wire("in1");
+	// 			in2 = Wire("in2");
+	// 			out = Wire();
+	// 			OrGate(in1,in2,out);
+	// 		});
+
+	// 		it("should start with out low", function() {
+	// 			expect(out("get-signal")).toBe(0)
+	// 		})
+
+	// 		describe("when in1 becomes high", function() {
+	// 			beforeEach(function() {
+	// 				in1("set-high")
+	// 			})
+
+	// 			it("the output should become high", function() {
+	// 				expect(out("get-signal")).toBe(1)
+	// 			})
+	// 		});
+
+	// 		describe("when in2 becomes high", function() {
+	// 			beforeEach(function() {
+	// 				in2("set-high")
+	// 			})
+
+	// 			it("the output should become high", function() {
+	// 				expect(out("get-signal")).toBe(1)
+	// 			})
+	// 		});
+	// 	});
+	// });
 })
-
-describe("an OR gate", function() {
-	var in1,in2,out;
-	describe("when in1 and in2 start low", function() {
-		beforeEach(function() {
-			in1 = Wire("in1");
-			in2 = Wire("in2");
-			out = Wire();
-			OrGate(in1,in2,out);
-		});
-
-		it("should start with out low", function() {
-			expect(out("get-signal")).toBe(0)
-		})
-
-		describe("when in1 becomes high", function() {
-			beforeEach(function() {
-				in1("set-high")
-			})
-
-			it("the output should become high", function() {
-				expect(out("get-signal")).toBe(1)
-			})
-		});
-
-		describe("when in2 becomes high", function() {
-			beforeEach(function() {
-				in2("set-high")
-			})
-
-			it("the output should become high", function() {
-				expect(out("get-signal")).toBe(1)
-			})
-		});
-	});
-});
