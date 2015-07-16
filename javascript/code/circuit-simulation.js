1efecff3227bc6c2e3cf44967a206646228f8137
Freek Paans <freekpaans@gmail.com>
2015-07-16 14:58:51 +0200

    lisping in js

--- javascript/code/circuit-simulation.js
+++ javascript/code/circuit-simulation.js
@@ -1,5 +1,45 @@
 console.log("loading circuit-simulation.js")
 
-module.exports = function() {
-	return true;
+module.exports.Wire = function() {
+	var signal = 0;
+
+	var actions = [];
+
+	var addAction = function(action) {
+		actions.push(action)
+	}
+
+	var setHigh = function() {
+		signal = 1;
+		actions.forEach(function(action) {
+			action();
+		})
+	}
+
+	return function(op) {
+		switch(op) {
+			case "set-high":
+				setHigh();
+				return;
+			case "get-signal":
+				return signal;
+			case "add-action":
+				addAction(arguments[1])
+				return;
+			default:
+				throw Error("Unknown msg "+op)
+
+		}
+	};
+}
+
+module.exports.OrGate = function(in1, in2, out) {
+	var onChange = function() {
+		if(in1("get-signal") || in2("get-signal")) {
+			out("set-high")
+		}
+	}
+	in1("add-action", onChange)
+	in2("add-action", onChange)
+	return function() {};
 }
--- javascript/spec/tests/circuit-simulation-spec.js
+++ javascript/spec/tests/circuit-simulation-spec.js
@@ -1,13 +1,47 @@
-var test = require('../../code/circuit-simulation.js');
+var circuit = require('../../code/circuit-simulation.js');
 
+var Wire = circuit.Wire;
+var OrGate=  circuit.OrGate;
 
-describe("a suite", function() {
-	it("should be null", function() {
-		expect(test()).toBe(false)	
+describe("a wire", function() {
+	it("should start low", function() {
+		var wire = Wire();
+		expect(wire("get-signal")).toBe(0)
 	})
+})
 
-	it("should be true", function() {
-		expect(true).toBe(true)	
-	})
+describe("an OR gate", function() {
+	var in1,in2,out;
+	describe("when in1 and in2 start low", function() {
+		beforeEach(function() {
+			in1 = Wire("in1");
+			in2 = Wire("in2");
+			out = Wire();
+			OrGate(in1,in2,out);
+		});
 
+		it("should start with out low", function() {
+			expect(out("get-signal")).toBe(0)
+		})
+
+		describe("when in1 becomes high", function() {
+			beforeEach(function() {
+				in1("set-high")
+			})
+
+			it("the output should become high", function() {
+				expect(out("get-signal")).toBe(1)
+			})
+		});
+
+		describe("when in2 becomes high", function() {
+			beforeEach(function() {
+				in2("set-high")
+			})
+
+			it("the output should become high", function() {
+				expect(out("get-signal")).toBe(1)
+			})
+		});
+	});
 });
